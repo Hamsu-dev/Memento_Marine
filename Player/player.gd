bd2ca33a06506662ee11870ae4a71283dff51b48
Sam <hhu354@aucklanduni.ac.nz>
2024-05-11 21:05:42 +1200

    Added slide

--- Player/player.gd
+++ Player/player.gd
@@ -1,22 +1,42 @@
 extends CharacterBody2D
 
-@export var acceleration = 1000  # Slightly lower for smoother acceleration
-@export var max_speed = 1000  # Increased for faster gameplay
+@export var acceleration = 400  # Slightly lower for smoother acceleration
+@export var max_speed = 250  # Increased for faster gameplay
 @export var friction = 1000  # Increased for quick stops
 @export var gravity = 500  # Reduced for lighter, longer jumps
 @export var jump_force = 200  # Increased for higher jumps
 @export var max_fall_velocity = 500  # Increased for more realistic falls
+@export var slide_speed = 400  # Speed during sliding
+@export var slide_duration = 0.5  # Duration of the slide in seconds
 
 
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var slide_timer = $SlideTimer
 
+var sliding = false
+
+#func _physics_process(delta):
+	#apply_gravity(delta)
+	#var input_axis = Input.get_axis("left", "right")
+	#if is_moving(input_axis):
+		#apply_acceleration(delta, input_axis)
+	#else:
+		#apply_friction(delta)
+	#jump_check()
+	#update_animations(input_axis)
+	#var was_on_floor = is_on_floor()
+	#move_and_slide()
+	#var just_left_edge = was_on_floor and not is_on_floor()
+	#if just_left_edge:
+		#coyote_jump_timer.start()
 
 func _physics_process(delta):
-	pass
 	apply_gravity(delta)
 	var input_axis = Input.get_axis("left", "right")
-	if is_moving(input_axis):
+	if sliding:
+		apply_slide(delta)
+	elif is_moving(input_axis):
 		apply_acceleration(delta, input_axis)
 	else:
 		apply_friction(delta)
@@ -27,6 +47,14 @@ func _physics_process(delta):
 	var just_left_edge = was_on_floor and not is_on_floor()
 	if just_left_edge:
 		coyote_jump_timer.start()
+	handle_slide_input(delta)  # Call to handle slide input
+
+func apply_slide(delta):
+	if animated_sprite_2d.flip_h:
+		velocity.x = -slide_speed
+	else:
+		velocity.x = slide_speed
+	velocity.y += gravity * delta  # Apply gravity even when sliding if needed
 
 
 func is_moving(input_axis):
@@ -39,7 +67,6 @@ func apply_gravity(delta):
 func apply_acceleration(delta, input_axis):
 	velocity.x = move_toward(velocity.x, input_axis * max_speed, acceleration * delta)
 
-
 func apply_friction(delta):
 	velocity.x = move_toward(velocity.x,  0, friction * delta)
 
@@ -51,6 +78,19 @@ func jump_check():
 		if Input.is_action_just_released("up") and velocity.y < -jump_force / 2:
 			velocity.y = -jump_force / 2
 
+func handle_slide_input(delta):
+	if Input.is_action_just_pressed("slide") and is_on_floor() and not sliding:
+		print("clicked shift")
+		start_slide()
+
+func start_slide():
+	sliding = true
+	slide_timer.start()
+
+func stop_slide():
+	sliding = false
+	slide_timer.stop()
+	
 func update_animations(input_axis):
 	var on_ground = is_on_floor()
 	
@@ -59,8 +99,11 @@ func update_animations(input_axis):
 	elif input_axis < 0:
 		animated_sprite_2d.flip_h = true
 		
-	# Handle jumping and falling
-	if not on_ground:
+	if sliding:
+		# If sliding, maintain the slide animation and do not change it
+		if animated_sprite_2d.animation != "slide":
+			animated_sprite_2d.play("slide")
+	elif not on_ground:
 		# If moving upwards
 		if velocity.y < 0:
 			animated_sprite_2d.play("jump")
@@ -74,3 +117,7 @@ func update_animations(input_axis):
 		# If on the ground and not moving
 		else:
 			animated_sprite_2d.play("idle")
+
+
+func _on_slide_timer_timeout():
+	stop_slide()

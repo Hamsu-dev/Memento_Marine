17dbe04476119c80889e8a76c0a429c62f485de4
Sam <hhu354@aucklanduni.ac.nz>
2024-05-23 22:30:14 +1200

    3 levels connected

---
+++ door_to_challenge.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+signal door_interact
+
+var player_in_area = false
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		player_in_area = true
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		player_in_area = false
+		
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("interact"):  # Assuming "ui_select" is mapped to the "E" key
+		SceneManager.change_scene("res://World/final_challenge.tscn")
